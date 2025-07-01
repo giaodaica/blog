023d8dd8762d0a3fed82819f3419a5337fa27665
Nguyễn Anh Quân <167054493+aquan17@users.noreply.github.com>
2025-07-01 11:44:14 +0700

    update menu lich su don hang

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\AddressBook;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use App\Models\Order;
 use App\Models\OrderItem;
 
@@ -13,12 +14,14 @@ class InfoController extends Controller
     public function updateProfile(Request $request)
     {
         $user = Auth::user();
-        $user->update($request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'default_phone' => 'nullable|string|max:20',
             'email' => 'required|email|unique:users,email,' . $user->id,
-        ]));
-        // dd($user);
+        ]);
+        
+        DB::table('users')->where('id', $user->id)->update($validatedData);
+        
         return redirect()->back()->with('success', 'Thông tin đã được cập nhật thành công!');
     }
 
@@ -26,6 +29,7 @@ class InfoController extends Controller
     {
         $user = Auth::user();
     
+        $addresses = AddressBook::where('user_id', $user->id)->limit(2)->get();
         $orders = Order::with([
             'orderItems.productVariant.color',
             'orderItems.productVariant.size',
@@ -33,11 +37,14 @@ class InfoController extends Controller
         ])
         ->where('user_id', $user->id)
         ->orderBy('created_at', 'desc')
-        ->get();
-    
-        $addresses = AddressBook::where('user_id', $user->id)->limit(2)->get();
-    
-        return view('pages.shop.account', compact('orders', 'addresses'));
+        ->get(); 
+        $pendingOrders = $orders->where('status', 'pending');
+        $confirmedOrders = $orders->where('status', 'confirmed');
+        $shippingOrders = $orders->where('status', 'shipping');
+        $completedOrders = $orders->where('status', 'completed');
+        $cancelledOrders = $orders->where('status', 'cancelled');
+        
+        return view('pages.shop.account', compact('orders', 'addresses', 'pendingOrders', 'confirmedOrders', 'shippingOrders', 'completedOrders', 'cancelledOrders'));
     }
    
 }
